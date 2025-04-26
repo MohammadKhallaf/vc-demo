d3069c6a2bffe279e3c7443a01acb4a331480bb3
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2025-04-26 14:35:54 +0300

    chore(src): remove duplicate log statement in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   console.log("App component rendered");
+
+  console.log("App component rendered");
   return (
     <>
       <div>
